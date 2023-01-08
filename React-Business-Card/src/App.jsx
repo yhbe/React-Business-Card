0880661d2afa4b24cf4ad516664ce0fc96420518
SHEF <Shefqetmush@gmail.com>
2023-01-08 17:39:40 -0500

    Finished footer

--- React-Business-Card/src/App.jsx
+++ React-Business-Card/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <div className="width-container">
         <Header />
+        <Footer />
       </div>
     </div>
   );
---
+++ React-Business-Card/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <ul className="footer-ul">
+        <li>
+          <i className="fa-brands fa-square-twitter"></i>
+        </li>
+        <li>
+          <i className="fa-brands fa-square-facebook"></i>
+        </li>
+        <li>
+          <i className="fa-brands fa-square-instagram"></i>
+        </li>
+        <li>
+          <i className="fa-brands fa-square-github"></i>
+        </li>
+      </ul>
+    </div>
+  );
+}
